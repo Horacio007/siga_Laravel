a1734a5a00893759a49e830b753b553fd70fb381
Horacio007 <jose097543@gmail.com>
2021-08-12 13:02:28 -0500

    en el monitor se agrega la opcion de sortear la columan 9 y 10 desc

--- public/js/administracion/monitor/monitor.js
+++ public/js/administracion/monitor/monitor.js
@@ -52,6 +52,7 @@ $(document).ready(function(){
         },
         select: true,
         pageLength: 100,
+        order: [[9, "desc"], [10, "desc"]],
         rowCallback: function(nRow, aData){
 
             if (aData[8] == 'Autorizado') {

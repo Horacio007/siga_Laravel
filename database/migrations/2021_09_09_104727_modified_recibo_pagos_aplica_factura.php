8058254b9503d533e25a13f0041405c4f51a7d8e
Horacio007 <jose097543@gmail.com>
2021-09-09 11:44:52 -0500

    se agrega el aplica factura osea la migracion

---
+++ database/migrations/2021_09_09_104727_modified_recibo_pagos_aplica_factura.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifiedReciboPagosAplicaFactura extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('recibo_pagos', function (Blueprint $table) {
+            $table->integer('aplica_factura')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
